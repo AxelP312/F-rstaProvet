e62d0401e5eb84b0fb651c2e51098345d97905f6
AxelP312 <axepetaxe@gmail.com>
2020-10-14 10:00:57 +0200

    Första

---
+++ FörstaProvet/Böckerna.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace FörstaProvet
+{
+    public class Böckerna
+    {
+    }
+}
---
+++ FörstaProvet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FörstaProvet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
